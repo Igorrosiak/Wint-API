0bd59a8a963e4ebcd23ec015e5fdfd3ce61c0daa
Igor Luiz Belter Rosiak <igorluizbelterrosiak@gmail.com>
2023-08-25 01:15:40 -0300

    init-spring-project

---
+++ src/main/java/com/wint/WintApplication.java
@@ -0,0 +1,13 @@
+package com.wint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wint/WintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
