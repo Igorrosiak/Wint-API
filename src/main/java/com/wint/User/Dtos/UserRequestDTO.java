881deeb0d05efad376085601cdb5242fb561361e
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-08-28 14:44:19 -0300

    fix-userDTOS

--- src/main/java/com/wint/User/Dtos/UserRequestDTO.java
+++ src/main/java/com/wint/User/Dtos/UserRequestDTO.java
@@ -2,5 +2,5 @@ package com.wint.User.Dtos;
 
 import jakarta.validation.constraints.NotNull;
 
-public record UserRequestDTO(@NotNull String name, @NotNull String email, @NotNull String password, @NotNull String birthDate, String presentation, String githubLink, String linkedinLink, String instagramLink, String facebookLink, String twitterLink, @NotNull String user) {
+public record UserRequestDTO(@NotNull String name, @NotNull String email, @NotNull String password, @NotNull String birthDate, String presentation, String githubLink, String linkedinLink, String instagramLink, String facebookLink, String twitterLink) {
 }
--- src/main/java/com/wint/User/Dtos/UserResponseDTO.java
+++ src/main/java/com/wint/User/Dtos/UserResponseDTO.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-public record UserResponseDTO(@NotNull UUID id, @NotNull String name, @NotNull String email, @NotNull String password, @NotNull String birthDate, String presentation, String githubLink, String linkedinLink, String instagramLink, String facebookLink, String twitterLink, @NotNull String user) {
+public record UserResponseDTO(@NotNull UUID id, @NotNull String name, @NotNull String email, @NotNull String password, @NotNull String birthDate, String presentation, String githubLink, String linkedinLink, String instagramLink, String facebookLink, String twitterLink) {
 }
