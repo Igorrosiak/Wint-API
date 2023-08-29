8761d48e5eec285a44d0c84aa1963ade78cb63e5
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-08-28 21:42:26 -0300

    run-prettier

--- src/main/java/com/wint/WintApplication.java
+++ src/main/java/com/wint/WintApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WintApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WintApplication.class, args);
-	};
+  public static void main(String[] args) {
+    SpringApplication.run(WintApplication.class, args);
+  }
 }
