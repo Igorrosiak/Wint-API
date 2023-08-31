78ec8cd7b8570e892c4e484f45f27b9687c15166
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-08-31 10:51:00 -0300

    creating-user-role

--- src/main/java/com/wint/User/Entitys/User.java
+++ src/main/java/com/wint/User/Entitys/User.java
@@ -3,64 +3,45 @@ package com.wint.User.Entitys;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Table(name = "users")
-@Entity(name = "user")
+@Entity(name = "users")
+@Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
 
   @Id
-  @Getter
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
-
-  @Getter
   @NotNull
   private String name;
-
-  @Getter
   @NotNull
   @Column(unique = true)
   private String email;
-
-  @Getter
   @NotNull
   private String password;
-
-  @Getter
   @NotNull
   private String birthDate;
-
-  @Getter
+  private UserRoleENUM role;
   private String presentation;
-
-  @Getter
   private String githubLink;
-
-  @Getter
   private String linkedinLink;
-
-  @Getter
   private String instagramLink;
-
-  @Getter
   private String facebookLink;
-
-  @Getter
   private String twitterLink;
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
   }
 
   @Override
