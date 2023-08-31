3ddc8c906137691163166d6129d828358f9c1fc2
igorrosiak <igorluizbelterrosiak@gmail.com>
2023-08-31 11:22:04 -0300

    inserting-role-on-registerDTO

--- src/main/java/com/wint/User/Dtos/RegisterDTO.java
+++ src/main/java/com/wint/User/Dtos/RegisterDTO.java
@@ -1,10 +1,12 @@
 package com.wint.User.Dtos;
 
+import com.wint.User.Entitys.UserRoleENUM;
 import jakarta.validation.constraints.NotNull;
 
 public record RegisterDTO(
   @NotNull String name,
   @NotNull String email,
   @NotNull String password,
-  @NotNull String birthDate
+  @NotNull String birthDate,
+  @NotNull UserRoleENUM role
 ) {}
